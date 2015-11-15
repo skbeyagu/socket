cffb374cfa6affc9a6378e6dceb7962b5af0f548
Santosh Kiran Beyagudem <skbeyagu@gmail.com>
2015-11-15 18:37:40 -0500

    updated index.html css

--- server.js
+++ server.js
@@ -10,6 +10,28 @@ app.use(express.static(__dirname+'/public'));
 
 var clientInfo = {};
 
+function sendCurrentUsers (socket){
+	var info = clientInfo[socket.id];
+	var users = [];
+
+	if(typeof info === 'undefined'){
+		return;
+	}
+
+	Object.keys(clientInfo).forEach(function(socketId){
+		var userInfo = clientInfo[socketId];
+		if (info.room = userInfo.room){
+			users.push(userInfo.name);
+		}
+	});
+
+	socket.emit('message', {
+		name: 'System',
+		text: 'Current Users: ' + users.join(', '),
+		timestamp: moment().valueOf()
+	});
+}
+
 io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
 	var userData = clientInfo[socket.id];
@@ -40,8 +62,16 @@ io.on('connection', function (socket) {
 
 	socket.on('message', function (message){
 		console.log("Message received: "+message.text);
-		message.timestamp = moment().valueOf();
-		io.to(userData.room).emit('message', message);
+
+		if (message.text === '@currentUsers'){
+			sendCurrentUsers(socket);
+
+		}else{
+			message.timestamp = moment().valueOf();
+			io.to(userData.room).emit('message', message);
+		}
+
+		
 	});
 
 
