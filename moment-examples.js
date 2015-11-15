b3c97a5fc8476d17392b280cf3dafbbbd02a2ec7
Santosh Kiran Beyagudem <skbeyagu@gmail.com>
2015-11-15 13:22:37 -0500

    show timestamp on UI

---
+++ moment-examples.js
@@ -0,0 +1,6 @@
+var moment = require('moment');
+var now = moment();
+
+console.log(now.format());
+console.log(now.format('X'));
+//console.log(now.format('MMM Do YYYY, h:mm a'));
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var moment = require('moment');
+
 
 app.use(express.static(__dirname+'/public'));
 
@@ -11,11 +13,15 @@ io.on('connection', function (socket) {
 
 	socket.on('message', function (message){
 		console.log("Message received: "+message.text);
+		message.timestamp = moment().valueOf();
 		io.emit('message',message);
 	});
 
+
+
 	socket.emit('message',{
-		text: 'Welcome to the Chat App'
+		text: 'Welcome to the Chat App',
+		timestamp: moment().valueOf()
 	});
 
 });
